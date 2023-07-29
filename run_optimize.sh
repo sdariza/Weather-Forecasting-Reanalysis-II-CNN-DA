e47fe27c7d8574b7df4b725c159b07fbe4ed995b
sdariza <sdariza@uninorte.edu.co>
2023-07-29 14:16:43 -0500

    feat: ✨ update run_optimizer.sh

--- run_optimize.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for i in {1..100}; do
-    echo "Executing optimize.py for $i"
-    python optimize.py --state="$1" --variable="$2"
-done
---
+++ run_optimizer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for i in {1..100}; do
+    echo "Executing optimize.py for $i"
+    python optimize.py --state="$1" --variable="air"
+    python optimize.py --state="$1" --variable="vwnd"
+    python optimize.py --state="$1" --variable="uwnd"
+done
