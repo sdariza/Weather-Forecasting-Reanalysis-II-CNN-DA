8e90eb72f844a4392ae2fcc266f87198ef3b5f5c
sdariza <sdariza@uninorte.edu.co>
2023-07-29 14:17:32 -0500

    feat: ✨ it will run 150 trials for all variables!

--- run_optimizer.sh
+++ run_optimizer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in {1..100}; do
+for i in {1..150}; do
     echo "Executing optimize.py for $i"
     python optimize.py --state="$1" --variable="air"
     python optimize.py --state="$1" --variable="vwnd"
