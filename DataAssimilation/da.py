b4deaf0fa9f1de6cb0a5237a65e7e40a2ed704cb
sdariza <sdariza@uninorte.edu.co>
2023-08-11 16:05:58 -0500

    feat: ✨ update da.py + add docstrings!

--- DataAssimilation/da.py
+++ DataAssimilation/da.py
@@ -1,72 +1,85 @@
-import argparse
+"""
+ This function is the work of my life :D
+"""
 import tensorflow as tf
-import netCDF4 as nc
 import numpy as np
 from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
 
-# parser = argparse.ArgumentParser(prefix_chars='--')
-# parser.add_argument('--state', type=int, help='State to predict')
-# parser.add_argument('--variable', type=str, help='Variable to predict')
+
+VARIABLE = 'air'
+
+model = [tf.keras.models.load_model(
+    f'./cnn-models/{state}/{VARIABLE}{state}.h5', compile=False) for state in [0, 6, 12, 18]]
 
 
-# args = parser.parse_args()
-# state = args.state
-# variable = args.variable
+def forecast(x_forecast, t_state: int):
+    """_summary_
 
+    Args:
+        x_forecast (_type_): _description_
+        t_state (int): _description_
 
-
-
-# states: 0 - 6 - 12 -18
-
-state = 6
-variable = 'air'
-nc_data_i = nc.Dataset(f'./test-data/0/air0.nc')
-nc_data_i_1 = nc.Dataset(f'./test-data/6/air6.nc')
-
-data_i = nc_data_i[f'{variable}'][:].data
-data_i_1 = nc_data_i_1[f'{variable}'][:].data
-
-nc_data_i.close()
-nc_data_i_1.close()
-
-xb = data_i[0].flatten()
-xr = data_i_1[0].flatten()
-n_members = 20
-
-def forecast(x,t):
-    x = x.reshape(73,144)
-    if t == 0:
-        states = [18, 0, 6, 12, 18]
-    elif t == 6:
-        states = [0, 6, 12, 18, 0]
-    elif t == 12:
-        states = [6, 12, 18, 0, 6]
+    Returns:
+        _type_: _description_
+    """
+    x_forecast = x_forecast.reshape(73, 144)
+    if t_state == 0:
+        states = [3, 0, 1, 2, 3]
+    elif t_state == 6:
+        states = [0, 1, 2, 3, 0]
+    elif t_state == 12:
+        states = [1, 2, 3, 0, 1]
     else:
-        states = [12, 18, 0, 6, 12]
-    for state in states:
-        model = tf.keras.models.load_model(f'./cnn-models/{state}/{variable}{state}.h5', compile=False)
-        x = model.predict(tf.reshape(x, [1, 73, 144, 1]))[0, ..., 0]
-        del model
-    return x.flatten()
-
-def create_initial_ensemble(xb, t, N):
-  Xb = []
-  for _ in range(0,N):
-    xbe_pert = xb+0.01*np.random.randn(73*144) #e-th pertured ensemble member (no consistent with model dynamics)
-    xbe = forecast(xbe_pert, t) #e-th ensemble member (consistent with model dynamics)
-    Xb.append(xbe)
-  return Xb
-
-def forecast_ensemble(Xb, t):
-  for e, _ in enumerate(Xb):
-    Xb[e,:] = forecast(Xb[e,:], t);
-  return Xb
+        states = [2, 3, 0, 1, 2]
+    for _ in range(1):
+        for state in states:
+            x_forecast = model[state].predict(tf.reshape(x_forecast, [1, 73, 144, 1]))[0, ..., 0]
+    return x_forecast.flatten()
 
 
-Xb = create_initial_ensemble(xb,6,n_members)
-Xb = np.array(Xb)
-xt0 = forecast(xb,6)
+def create_initial_ensemble(x_b, t_state, n_members):
+    """_summary_
+
+    Args:
+        x_b (_type_): _description_
+        t_state (_type_): _description_
+        n_members (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    ensemble = []
+    for _ in range(0, n_members):
+        # e-th pertured ensemble member (no consistent with model dynamics)
+        xbe_pert = x_b+0.01*np.random.randn(73*144)
+        # e-th ensemble member (consistent with model dynamics)
+        xbe = forecast(xbe_pert, t_state)
+        ensemble.append(xbe)
+    return np.array(ensemble)
+
+
+def forecast_ensemble(ensemble, t_state):
+    """_summary_
+
+    Args:
+        ensemble (_type_): _description_
+        t_state (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    for element, _ in enumerate(ensemble):
+        ensemble[element, :] = forecast(ensemble[element, :], t_state)
+    return Xb
+
+
+NMEMBERS = 20
+x_random = np.random.randn(73*144,)
+x_0 = forecast(x_random, 6)
+x_b0 = forecast(x_0, 6)
+x_t = forecast(x_0, 6) # reference
+Xb = create_initial_ensemble(x_b0, 6, NMEMBERS)
 lons = np.linspace(0, 357.5, 144)
 lats = np.linspace(90, -90, 73)
 
@@ -76,46 +89,11 @@ lon, lat = np.meshgrid(lons, lats)
 min_lat, max_lat, min_lon, max_lon = lats.min(), lats.max(), lons.min(), lons.max()
 
 m = Basemap(projection='cyl', resolution='c',
-                llcrnrlat=min_lat, urcrnrlat=max_lat,
-                llcrnrlon=min_lon, urcrnrlon=max_lon)
+            llcrnrlat=min_lat, urcrnrlat=max_lat,
+            llcrnrlon=min_lon, urcrnrlon=max_lon)
 
 m.drawcoastlines(linewidth=0.5)
-cax = m.contourf(lon, lat,Xb.mean(axis=0).reshape(73,144), levels=100)
+cax = m.contourf(lon, lat, Xb.mean(axis=0).reshape(73, 144), levels=100)
 plt.savefig('DataAssimilation/Xb.png')
+plt.show()
 plt.close()
-
-# p = 0.8; #[0, 1] 0: no observations and 1: full observational network
-# m = round(p*73*144); #number of observations m<=n
-# err_obs = 0.01; #standard deviations of errors (a typical value)
-# R = (err_obs**2)*np.eye(m, m); #covariance matrix of data errors
-# M = 5; #number of assimilation cycles
-# t = [0, 0.1]
-# xt = np.array(xt0).astype('float32');
-# Xa = np.array(Xb.copy()).astype('float32')
-# err_a = [];
-# err_b = [];
-
-# for k in range(M):
-#   #Forecast step
-#   xt = forecast(xt, 0)
-#   Xb = forecast_ensemble(Xa, t)
-#   Pb = np.cov(Xb.T)
-#   xb = np.mean(Xb, axis=0)
-#   err_b.append(np.linalg.norm(xb-xt))
- 
-#   #Create observation - data for the assimilation process
-#   H = np.random.permutation(np.arange(73*144))[:m]; #we observe a different part of the domain
-#   y = np.random.multivariate_normal(xt[H], R);
-
-#   #Assimilation step
-#   Ys = np.random.multivariate_normal(y, R, N).T; #synthetic observations
-#   Ds = Ys - Xb[:,H].T; #Synthetic innovations
-#   Pa = R + Pb[H,:][:,H]; #Pa = R + H @ Pb @ H.T
-#   Za = np.linalg.solve(Pa, Ds);
-#   DX = Pb[:,H] @ Za;
-#   Xa = Xb + DX.T;
-#   xa = np.mean(Xa, axis=0);
-#   err_a.append(np.linalg.norm(xa-xt));
-
-# plt.plot(np.log(np.array(err_b)), '-b')
-# plt.plot(np.log(np.array(err_a)), '-r')
