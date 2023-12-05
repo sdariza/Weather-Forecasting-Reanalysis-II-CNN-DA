fa2d58a7e9f8b75d6e3e9bedc5b0796da920aa22
sdariza <sdariza@uninorte.edu.co>
2023-12-05 13:19:08 -0500

    feat: ✨update run_optimizer.sh 1 to 200

--- data_driven/run_optimizer.sh
+++ data_driven/run_optimizer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in {98..200}; do
+for i in {1..200}; do
     echo "Executing optimize.py for $i"
     python data_driven/optimize.py --state="$1" --variable="air"
     python data_driven/optimize.py --state="$1" --variable="vwnd"
